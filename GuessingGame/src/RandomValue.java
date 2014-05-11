5125a92229088ad8a708b7d2505a5144d63905d9
aaltintop <altintop@ngine.com.tr>
2014-05-11 11:02:25 -0430

    selam

--- GuessingGame/src/RandomValue.java
+++ GuessingGame/src/RandomValue.java
@@ -1,56 +1,56 @@
 import java.util.Random;
 
-/*main de sadece randomValueControl metotunu kullancagÄ±z.
- * bundan return olan deger rakamlarÄ± farklÄ± 4basamaklÄ± sayÄ±dÄ±r.
- * bu metot RandomValueProduce metotunu iÃ§inde bulunduruyor. RandomValueProduce metotu ise random deger uretir.*/ 
+/*main de sadece randomValueControl metotunu kullancagýz.
+* bundan return olan deger rakamlarý farklý 4basamaklý sayýdýr.
+* bu metot RandomValueProduce metotunu içinde bulunduruyor. RandomValueProduce metotu ise random deger uretir.*/
 public class RandomValue {
-	private int value;
-	public RandomValue()
-	{
-    
-	}
-	public int randomValueControl()//bu metotun amacÄ± sayÄ± iÃ§inde aynÄ± olan rakam varsa o sayÄ±yÄ± degiÅŸtirip tekrar random deger atar. 
-	{
-		boolean son=true;
-		RandomValueProduce();
-		do
-			{
-			son=true;
-			String [] series=new String[4];
-			
-		int lenght=4;
-		for(int i=3;i>=0;i--)
-		{
-			series[i]=(Integer.toString(value).substring(lenght-1,lenght));
-			lenght--;
-		}
-		for (int i = 0; i < 4; i++) {
-			  for (int j = 0; j < 4; j++) {
-				if(series[j].equals(series[i]) && i!=j)
-				{
-					son=RandomValueProduce();
-					break;
-				}
-					
-			}
-			  if(son==false)
-				  break;
+private int value=0;//abd
+public RandomValue()
+{
+    //selam
+}
+public int randomValueControl()//bu metotun amacý sayý içinde ayný olan rakam varsa o sayýyý degiþtirip tekrar random deger atar.
+{
+boolean son=true;
+RandomValueProduce();
+do
+{
+son=true;
+String [] series=new String[4];
 
-		}
-		//System.err.println(value);//bu satÄ±rÄ± comment den cÄ±karÄ±rsak consol ekranÄ±nda random sayÄ±nÄ±n kaÃ§ oldugu yazar.
-			}while(son==false);
+int lenght=4;
+for(int i=3;i>=0;i--)
+{
+series[i]=(Integer.toString(value).substring(lenght-1,lenght));
+lenght--;
+}
+for (int i = 0; i < 4; i++) {
+for (int j = 0; j < 4; j++) {
+if(series[j].equals(series[i]) && i!=j)
+{
+son=RandomValueProduce();
+break;
+}
 
-		return value;
-	}
-	private boolean RandomValueProduce() {
-		Random rnd = new Random();
+}
+if(son==false)
+break;
+
+}
+//System.err.println(value);//bu satýrý comment den cýkarýrsak consol ekranýnda random sayýnýn kaç oldugu yazar.
+}while(son==false);
+
+return value;
+}
+private boolean RandomValueProduce() {
+Random rnd = new Random();
         value = rnd.nextInt(9000)+1000;
-		return false;
-	}
-	public int getValeu() {
-		return value;
-	}
-	public void setValue(int value) {
-		this.value = value;
-	}
+return false;
+}
+public int getValeu() {
+return value;
+}
+public void setValue(int value) {
+this.value = value;
+}
 }
